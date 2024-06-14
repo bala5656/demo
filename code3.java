9848e2e4971f9ed2a07acc00f14b030498b93610
unknown <vishnubalapandiyan@gmail.com>
2024-06-14 07:57:09 -0700

    modified code3.java

--- code3.java
+++ code3.java
@@ -3,5 +3,12 @@ public class{
     public static void main (String[]args){
  
         System.out.println("hello world...java!");
+
+        public class case1{
+
+            int num = 18;
+        }
+
+        System.out.println("num :" num);
     }
 }
